0a1c7ed51c11a69f35d0bf70a0c3767e275d174d
Julfikar Ali <julfikarali.282@gmail.com>
2026-09-12 12:28:44 +0600

    Add swagger and Task Model

---
+++ Model/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskFlowAPI.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Register swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My Web API",
+        Version = "v1",
+        Description = "API documentation created using Swashbuckle"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwagger();   // Generate Swagger JSON (/swagger/v1/swagger.json)
+    app.UseSwaggerUI(); // Display Interactive UI  (/swagger)
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
